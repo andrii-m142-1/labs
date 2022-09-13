f0be0f4107d30cf2d17434a75c688c000aec628a
andrii-m142-1 <hovorukha.a.y@nmu.one>
2022-09-13 13:49:22 +0300

    добавил класс и запустил его из мейна другого класса

--- src/main/java/lab0/lab0.java
+++ src/main/java/lab0/lab0.java
@@ -2,7 +2,12 @@ package lab0;
 
 public class lab0 {
 
-    public String vivod() {
+    public static void main(String[] args)
+    {
+        String a=vivod();
+        System.out.println("lab0_main:"+a);
+    }
+    public static  String vivod() {
         System.out.println("Hello World!");
         String abc = "hello";
         return abc;
